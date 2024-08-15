063cc07b436c989bf0fbb120f58872bcd65068c9
Rohitsah6372 <91113994+Rohitsah6372@users.noreply.github.com>
2024-08-15 09:41:51 +0530

    first commit

--- src/main/java/com/driver/StudentRepository.java
+++ src/main/java/com/driver/StudentRepository.java
@@ -19,40 +19,56 @@ public class StudentRepository {
 
     public void saveStudent(Student student){
         // your code goes here
+        studentMap.put(student.getName(), student);
     }
 
     public void saveTeacher(Teacher teacher){
         // your code goes here
+        teacherMap.put(teacher.getName(), teacher);
     }
 
     public void saveStudentTeacherPair(String student, String teacher){
         if(studentMap.containsKey(student) && teacherMap.containsKey(teacher)){
-            // your code goes here
+            teacherStudentMapping
+                    .computeIfAbsent(teacher, k -> new ArrayList<>())
+                    .add(student);
         }
     }
 
     public Student findStudent(String student){
         // your code goes here
+        return studentMap.get(student);
     }
 
     public Teacher findTeacher(String teacher){
         // your code goes here
+        return teacherMap.get(teacher);
     }
 
     public List<String> findStudentsFromTeacher(String teacher){
         // your code goes here
         // find student list corresponding to a teacher
+        return teacherStudentMapping.getOrDefault(teacher, Collections.emptyList());
     }
 
     public List<String> findAllStudents(){
         // your code goes here
+        List<String> students = new ArrayList<>();
+        for (List<String> str : teacherStudentMapping.values()) {
+            for (String student : str) {
+                students.add(student);
+            }
+        }
+        return students;
     }
 
     public void deleteTeacher(String teacher){
         // your code goes here
+        teacherMap.remove(teacher);
     }
 
     public void deleteAllTeachers(){
         // your code goes here
+        teacherMap.clear();
     }
 }
